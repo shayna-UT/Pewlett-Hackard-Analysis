70b369828be01254c4237a95e7454e56245b1db3
Shayna Sims <shaynarsims@yahoo.com>
2020-07-19 11:51:04 -0500

    update queries on retirement info

--- queries.sql
+++ queries.sql
@@ -37,3 +37,124 @@ AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
 
 -- See what the table looks like
 SELECT * FROM retirement_info;
+
+-- Drop original retirement_info table
+DROP TABLE retirement_info;
+
+-- Create new table for retiring employees to incl. emp_no
+SELECT emp_no, first_name, last_name
+INTO retirement_info
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+SELECT * FROM retirement_info;
+
+-- Joining departments and dept_manager tables
+SELECT d.dept_name,
+	dm.emp_no,
+	dm.from_date,
+	dm.to_date
+FROM departments AS d
+INNER JOIN dept_manager AS dm
+ON d.dept_no = dm.dept_no;
+
+-- Joining retirement_info and dept_emp tables
+-- to create table of current employees about to retire
+SELECT ri.emp_no,
+	ri.first_name,
+	ri.last_name,
+	de.to_date
+INTO current_emp
+FROM retirement_info AS ri
+LEFT JOIN dept_emp AS de
+ON ri.emp_no = de.emp_no
+WHERE de.to_date = ('9999-01-01');
+
+SELECT * FROM current_emp;
+
+-- Employee count by department number
+SELECT COUNT(ce.emp_no), de.dept_no
+INTO emp_count
+FROM current_emp as ce
+LEFT JOIN dept_emp as de
+ON ce.emp_no = de.emp_no
+GROUP BY de.dept_no
+ORDER BY de.dept_no;
+
+SELECT * FROM emp_count;
+
+-- List of Employee Information
+-- for current employees about to retire
+-- include: emp_no, last name, first name, gender, salary 
+SELECT * FROM salaries
+ORDER BY to_date DESC;
+
+SELECT e.emp_no, 
+	e.first_name, 
+	e.last_name, 
+	e.gender,
+	s.salary,
+	de.to_date
+INTO emp_info
+FROM employees as e
+	INNER JOIN salaries as s
+		ON (e.emp_no = s.emp_no)
+	INNER JOIN dept_emp as de
+		ON (e.emp_no = de.emp_no)
+	WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+		AND (e.hire_date BETWEEN '1985-01-01' AND '1988-12-31')
+		AND (de.to_date = '9999-01-01');
+		
+SELECT * FROM emp_info;
+
+-- List of Managers per Department
+-- for current employees about to retire
+-- include: dept_no, dept_name, emp_no, last name, first name, starting & ending employment
+SELECT dm.dept_no,
+	d.dept_name,
+	dm.emp_no,
+	ce.last_name,
+	ce.first_name,
+	dm.from_date,
+	dm.to_date
+INTO manager_info
+FROM dept_manager as dm
+	INNER JOIN departments as d
+		ON (dm.dept_no = d.dept_no)
+	INNER JOIN current_emp as ce
+		ON (dm.emp_no = ce.emp_no);
+		
+SELECT * FROM manager_info;
+
+-- List of Department Retirees
+-- include: emp_no, first name, last name, dept_name
+SELECT ce.emp_no,
+	ce.first_name,
+	ce.last_name,
+	d.dept_name
+INTO dept_info
+FROM current_emp as ce
+	INNER JOIN dept_emp as de
+		ON (ce.emp_no = de.emp_no)
+	INNER JOIN departments as d
+		ON (de.dept_no = d.dept_no);
+
+SELECT * FROM dept_info;
+
+-- Sales Team Retirement Info
+SELECT *
+INTO sales_info
+FROM dept_info
+WHERE dept_name IN ('Sales');
+
+SELECT * FROM sales_info;
+
+-- Mentorship Info
+SELECT *
+INTO mentor_info
+FROM dept_info
+WHERE dept_name IN ('Sales', 'Development');
+
+SELECT * FROM mentor_info;
+
